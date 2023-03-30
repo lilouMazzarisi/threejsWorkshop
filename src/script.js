f59c9ba0fa52bba4aa2b1f2f02457b965b29aca8
Lilou Mazzarisi <lisa_mazzarisi@hotmail.com>
2023-03-30 10:53:59 +0200

    end first part

--- src/script.js
+++ src/script.js
@@ -1,1 +1,68 @@
-console.log("wat up");
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+// SCENE
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 'blue', wireframe: true })
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+// CAMERA
+//Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    //renderer.render(scene, camera)
+})
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.set(0, 0, 3)
+scene.add(camera)
+
+// RENDER
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+// Orbit Control
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// ANIMATION
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+    // console.log(elapsedTime)
+    mesh.rotation.y = elapsedTime * Math.PI
+    mesh.position.y = Math.sin(elapsedTime * 2)
+    mesh.position.x = Math.cos(elapsedTime * 2)
+    mesh.rotation.x = 0.5 * elapsedTime
+
+    controls.update()
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+tick()
